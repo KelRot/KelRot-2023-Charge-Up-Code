34bf9b8d6dd2bccee3a8018dfa6ca3c5c20e7d4b
unknown <fings.nft@gmail.com>
2023-03-13 09:41:54 +0300

    charging station pid added

---
+++ Trajectroy Following/src/main/java/frc/robot/commands/ChargingStation.java
@@ -0,0 +1,40 @@
+package frc.robot.commands;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.ChargingConstants;
+import frc.robot.subsystems.Drive;
+
+public class ChargingStation extends CommandBase {
+  private final Drive m_drive;
+  private final PIDController m_pid;
+  public ChargingStation(Drive drive) {
+    m_drive = drive;
+
+    m_pid = new PIDController(ChargingConstants.kP, ChargingConstants.kI, ChargingConstants.kD);
+    m_pid.setTolerance(3, 5);
+    m_pid.setIntegratorRange(-0.5, 0.5);
+    
+    addRequirements(m_drive);
+  }
+
+  @Override
+  public void initialize() {
+    m_drive.setMaxOutput(4);
+    m_pid.reset();
+  }
+
+  @Override
+  public void execute() {
+    double volts = m_pid.calculate(m_drive.getAngle(), 0);
+    m_drive.tankDriveVolts(volts, volts);
+  }
+
+  @Override
+  public void end(boolean interrupted) {}
+
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
